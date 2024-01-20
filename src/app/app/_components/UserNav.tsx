8cc359017ee26b841ba2f4a9b39961386c932978
Tarasikee <taras@deepinsight.io>
2024-01-20 19:07:39 +0100

    Better ui

--- src/app/app/_components/UserNav.tsx
+++ src/app/app/_components/UserNav.tsx
@@ -4,28 +4,34 @@ import {
     DropdownMenu,
     DropdownMenuTrigger,
     DropdownMenuContent,
-    DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuGroup, DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuGroup,
+    DropdownMenuItem,
 } from '~/components/ui/dropdown-menu'
 import { Button } from '~/components/ui/button'
 import { Avatar, AvatarImage, AvatarFallback } from '~/components/ui/avatar'
+import { useSession, signOut } from 'next-auth/react'
 
 export function UserNav() {
+    const session = useSession()
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="/avatars/01.png" alt="@shadcn"/>
-                        <AvatarFallback>SC</AvatarFallback>
+                        <AvatarImage src={session.data?.user?.image ?? ''} alt="@shadcn"/>
+                        <AvatarFallback>{session.data?.user?.name?.[0]}</AvatarFallback>
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
                     <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">shadcn</p>
+                        <p className="text-sm font-medium leading-none">{session.data?.user?.name}</p>
                         <p className="text-xs leading-none text-muted-foreground">
-                            m@example.com
+                            {session.data?.user?.email}
                         </p>
                     </div>
                 </DropdownMenuLabel>
@@ -42,7 +48,7 @@ export function UserNav() {
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator/>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={() => signOut()}>
                     Log out
                 </DropdownMenuItem>
             </DropdownMenuContent>
